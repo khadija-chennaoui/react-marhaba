f60b2e99c9f8a577ec0393834bb55b4107501452
khadija-chennaoui <khadijach896@gmail.com>
2022-11-01 11:48:24 +0100

    test

---
+++ src/components/Navbars/test.js
@@ -0,0 +1,11 @@
+// import  { React , useState } from "react";
+
+//  function test(e){
+//     const [user, setUser] = useState({});
+//     const val = e.target.value;
+//     setUser({
+//     ...user,
+//     [e.target.name]: val,
+//     });
+// }
+// export default test;
